062bf3fe78b22444f01a1c73ae1852a8ccc90a2b
Jason Yandell <jyandell@casham.com>
2012-10-20 14:28:39 -0500

    Yeoman AngularJS scaffolding

---
+++ app/scripts/Gitlinx.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var GitlinxApp = angular.module('GitlinxApp', [])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
